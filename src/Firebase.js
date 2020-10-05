9d02877ed48fe0548606ada583c25bdcbf842f76
steven payne <djnosey@hotmail.co.uk>
2020-10-05 15:42:15 +0200

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCX1SB8Z31Y0R1lGZkveRnVLxnZc5r1Kv8",
+    authDomain: "whatsapp-d12d1.firebaseapp.com",
+    databaseURL: "https://whatsapp-d12d1.firebaseio.com",
+    projectId: "whatsapp-d12d1",
+    storageBucket: "whatsapp-d12d1.appspot.com",
+    messagingSenderId: "861087884620",
+    appId: "1:861087884620:web:c53b9b29c4523f57249be9"
+  };
